b6904164ed4677c1226a2ac33ac9136be6ab90db
kcavagnolo <kencavagnolo@gmail.com>
2015-06-12 15:55:00 -0400

    Changes

--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
 # todo...
 # crawl bill tree and hash all final bill versions
-# store all words per congress into one file
-# or keep in psql
+# store all words per congress into one file or keep in psql
